1ffc909dba05b1e6ef62afc02d8e7276190ac5cf
Sebastiaan Deckers <cbas@pandion.be>
2012-03-05 06:44:03 +0800

    Refactoring

--- js/script.js
+++ js/script.js
@@ -1,56 +1,11 @@
-/** Author: Sebastiaan Deckers <sebdeckers83@gmail.com>
- ** Ruined by: Veronica <veronica@aurorae.sg>
+/** Authors: Sebastiaan Deckers <sebdeckers83@gmail.com>,
+ **          Veronica <veronica@aurorae.sg>
  **/
-
-/* Navigation Menu
- */
-/* * /
-(function() {
-	var updateMenu = function(event) {
-			var links = $("body > header > .masthead > nav > ul a");
-			links.parent().filter(".active").removeClass("active");
-			links
-				.filter(function() { return location.hash === $(this).attr("href"); })
-				.parent()
-				.addClass("active");
-			if(!links.parent().hasClass("active")) {
-				links.parent().first().addClass("active");
-			}
-			event && event.preventDefault();
-		};
-		updateMenu();
-		window.addEventListener("hashchange", updateMenu, false);
-})();
-$("body > header > .masthead > nav > ul").on("click", "a", function(event) {
-	var link = $(event.target);
-	if(link.parent().is(":not(.active)")) {
-		link.closest("ul").find(".active").removeClass("active");
-		link.parent().addClass("active");
-	}
-});
-$("header nav").on("click", "a[href^=#]", function(event){
-	var margin = 55,
-		name = $(this).attr("href").substr(1),
-		position = name.length
-			? $("body, a[name=" + name + "], #" + name).last().offset().top
-			: 0;
-	$("body").animate({scrollTop: Math.max(0, position - margin)});
-	location.hash = "#" + name;
-	event.preventDefault();
-});
-/* */
 
 /* Highlight External Links
  */
-function externalLinks() {
-	if (!document.getElementsByTagName)
-	return; var anchors = document.getElementsByTagName("a");
-	
-	for (var i=0; i<anchors.length; i++) {
-		var anchor = anchors[i]; if (anchor.getAttribute("href") && anchor.getAttribute("rel") == "external") anchor.target = "_blank";
-	}
-} window.onload = externalLinks;
- 
+(function() { $("a[href][rel=external]").attr({target: "_blank"}); })();
+
 /* Firm Practices
  */
 $("#firm > aside li:not(.active) .snippet").hide();
