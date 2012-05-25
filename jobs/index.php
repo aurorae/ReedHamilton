5bbeae2093a411ce69c764faa746110d673f30f7
Veronica <hello@v.eroni.ca>
2012-05-25 14:05:40 +0800

    Pulled from Reedhamilton.com

---
+++ jobs/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>ReedHamilton</title>
+<script type="text/javascript" src="http://www.reedhamilton.com/Scripts/jquery-1.5.1.js"></script>
+<style>
+html {
+	height: 100%;
+}
+body {
+	height: 100%;
+	margin: 0px;
+	padding: 0px;
+	border: 0;
+	overflow: hidden;
+}
+
+iframe {
+	height: 100%;
+	margin: 0px;
+	padding: 0px;
+	border: 0;
+	text-align: center;
+	overflow-x: hidden;
+	overflow-y: auto;
+}
+</style>
+</head>
+
+<body>
+<iframe id="container" src="http://reedhamilton.catsone.com/careers/index.php?m=portal&portalID=8549" scrolling="yes" width="100%" frameborder="0" marginwidth="0" marginheight="0" />
+<span id="test"></span>
+</body>
+</html>
---
+++ owa/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>ReedHamilton</title>
+<script type="text/javascript" src="http://www.reedhamilton.com/Scripts/jquery-1.5.1.js"></script>
+<script type="text/javascript">
+$(document).ready(
+	function() {
+		var v = $(window).height();
+		$('iframe#container').css('height', v);
+	}
+);
+</script>
+<style>
+body {
+	margin: 0px;
+	padding: 0px;
+	border: 0;
+	overflow: hidden;
+}
+
+iframe {
+	margin: 0px;
+	padding: 0px;
+	border: 0;
+	text-align: center;
+	overflow-x: hidden;
+	overflow-y: auto;
+}
+</style>
+</head>
+
+<body>
+<iframe id="container" src="https://webmail.hostallapps.com/owa/auth/logon.aspx?replaceCurrent=1&url=https%253a%252f%252fwebmail.hostallapps.com%252fowa%252f" scrolling="yes" width="100%" frameborder="0" marginwidth="0" marginheight="0" />
+</body>
+</html>
